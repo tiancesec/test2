c7a47686f558d1e1eac566a8dcced4a27489061e
tiancesec <74537176+tiancesec@users.noreply.github.com>
2023-08-08 22:22:43 +0800

    Update environment.py

--- modules/environment.py
+++ modules/environment.py
@@ -3,10 +3,3 @@ import os
 def run(**args):
     print("[*] In environment module.")
     return str(os.environ)
-
-
-def main():
-    run()
-    os.environ=str(os.environ)
-    print(os.environ)
-main()
